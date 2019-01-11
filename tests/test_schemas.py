44e55c38ab7d43cb5d75dd6820f7c271d9107cb2
maudehrmann <maud.ehrmann@gmail.com>
2019-01-11 13:48:04 +0100

    test for python lib compatibility with json schema versions

---
+++ tests/test_schemas.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8 
+
+import python_jsonschema_objects as pjs
+import pkg_resources
+import os
+import json
+
+
+def test_get_issue_schema():
+
+    schema_path = "../json/newspaper/issue.schema.json"
+    example_path = "../examples/newspaper_issue/example0.json"
+
+    # loading the schema
+    with open(os.path.join(schema_path), 'r') as f:
+        json_schema = json.load(f)
+    builder = pjs.ObjectBuilder(json_schema)
+    IssueSchema = builder.build_classes().NewspaperIssue
+
+    # loading and validating the issue
+    with open(os.path.join(example_path), 'r') as f:
+        json_example = json.load(f)
+    issue = IssueSchema(**json_example)
+    t = issue.validate()
+    assert t is True
+    #print(issue.serialize())
